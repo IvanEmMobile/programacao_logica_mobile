7fef446cb113fe02161cdc31c5da40852758a02b
Ivan Paulino <ivanemmobile@hotmail.com>
2024-05-19 00:52:27 -0300

    Projeto Snake Run 2
    Projeto em Java Script para criar um jogo de snake run com base no repositório: @manualdodev

--- SnakeRun2-AvaliaçãoFinal-SeuNome/assents/script.js
+++ SnakeRun2-AvaliaçãoFinal-SeuNome/assents/script.js
@@ -1,253 +1,254 @@
-window.onload = function(){
- 
-  var stage = document.getElementById('stage');
-  var ctx = stage.getContext("2d");
-  document.addEventListener("keydown", keyPush);
-  setInterval(game, 80);
-
-  const vel = 1;
-
-  var vx = vy = 0;
-  var px =10;
-  var py = 15;
-  var tp = 30;
-  var qp = 20;
-  var ax=ay=15;
-
-  var trail = [];
-  tail = 5;
-
-  function game(){
-      px += vx;
-      py += vy;
-      if (px <0) {
-          px = qp-1;
-      }
-      if (px > qp-1) {
-          px = 0;
-      }
-      if (py < 0) {
-          py = qp-1;
-      }
-      if (py > qp-1) {
-          py = 0;
-      }
-
-      ctx.fillStyle = "black";
-      ctx.fillRect(0,0, stage.width, stage.height);
-
-      ctx.fillStyle = "red";
-      ctx.fillRect(ax*tp, ay*tp, tp,tp);
-
-      ctx.fillStyle = "gray";
-      for (var i = 0; i < trail.length; i++) {
-          ctx.fillRect(trail[i].x*tp, trail[i].y*tp, tp-1,tp-1);
-          if (trail[i].x == px && trail[i].y == py)
-          {
-              vx = vy=0;
-              tail =5;
-          }
-      }
-
-      trail.push({x:px,y:py })
-      while (trail.length > tail) {
-          trail.shift();
-      }
-
-      if (ax==px && ay==py){
-          tail++;
-          ax = Math.floor(Math.random()*qp);
-          ay = Math.floor(Math.random()*qp);
-      }
-
-  }
-
-  function keyPush(event){
-
-      switch (event.keyCode) {
-          case 37: // Left
-              vx = -vel;
-              vy = 0;
-              break;
-          case 38: // up
-              vx = 0;
-              vy = -vel;
-              break;
-          case 39: // right
-              vx = vel;
-              vy = 0;
-              break;
-          case 40: // down
-              vx = 0;
-              vy = vel;
-              break;          
-          default:
-              
-              break;
-      }
-
-
-  }
-
-}
-
-/*
-
-//-------------------------------------------
+//Definir o tipo de canvas, jogo 2D - Snake Run 2
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-const score =document.querySelector(".score--value")
+
+//Definido a visualização do score, exibir play
+const score = document.querySelector(".score--value")
 const finalScore = document.querySelector(".final-score > span")
 const menu = document.querySelector(".menu-screen")
 const buttonPlay = document.querySelector(".btn-play")
 
 
+
+//definindo o tamanho da Snake
 const size = 30
 
-const snake = [
-  {x: 270, y: 240},
-  {x: 300, y: 240},
-  {x: 330, y: 240},
-  {x: 360, y: 240},
-  {x: 390, y: 240},
-  {x: 420, y: 240},
-]
+
+
+//criando a Snake por array, sendo preenchido e movimentado na tela
+const initialPosition = { x: 270, y: 240 }
+let snake = [initialPosition]
+
+//Forma Anterior de criar a Snake Run
+/*const snake = [
+  { x: 300, y: 240 },
+  { x: 330, y: 240 },
+  { x: 360, y: 240 }
+]*/
+
+
 
 const incrementScore = () => {
-  score.innerText = parseInt(score.innerText) + 10
+  score.innerText = + score.innerText + 10
 }
 
-const randomNumber = (min, max) =>{
-  return Math.floor(Math.random() * (max - min) + min)
+
+
+//Criar uma posição aleatoria da Apple
+const randomNumber = (min, max) => {
+  return Math.round(Math.random() * (max - min) + min)
 }
 
+
+
+//Definir uma posição da Appple dentro do canvas
 const randomPosition = () => {
   const number = randomNumber(0, canvas.width - size)
-  return Math.round(number / 30 * 30)
+  return Math.round(number / 30) * 30
+}
+
+
+const randomColor = () => {
+  const red = randomNumber(0, 255)
+  const green = randomNumber(0, 255)
+  const blue = randomNumber(0, 255)
+
+  return `rgb(${red}, ${green}, ${blue})`
 }
 
 
 
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
+//Criando a const para a Apple, Sempre Laranja
+const food = {
+  x: randomPosition(),
+  y: randomPosition(),
+  color: randomColor()
+}
+
+let direction, loopId
+
+//definindo a posição inicial da Apple
+const drawFood = () => {
+  const { x, y, color } = food
+
+  ctx.shadowColor = color
+  ctx.shadowBlur = 20
+  ctx.fillStyle = color
+  ctx.fillRect(x, y, size, size)
+  ctx.shadowBlur = 0
+}
 
 
 
-//-------------------------------------------
-window.onload = function () {
-  canvas = document.getElementById("canvas");
-  ctx = canvas.getContext("2d");
-  document.addEventListener("keydown", keyDownEvent);
-  let x = 8;
-  setInterval(desenharJogo, 1000 / x);
-};
+//A const desenhSnake recebe os elementos e cria um começo da Snake
+const drawSnake = () => {
+  ctx.fillStyle = "#593493"
 
-let canvas;
-let ctx;
+  snake.forEach((position, index) => {
+    if (index == snake.length - 1) {
+      ctx.fillStyle = "#844CA3"
+    }
 
-//Tratamento de Teclas
-function keyDownEvent(event) {
-  // nextX e nextY representam as direções que a cobra irá percorrer
-  // nos eixos X e Y, respectivamente
-  switch (event.keyCode) {
-    case 37:
-      nextX = -1;
-      nextY = 0;
-      break;
-    case 38:
-      nextX = 0;
-      nextY = -1;
-      break;
-    case 39:
-      nextX = 1;
-      nextY = 0;
-      break;
-    case 40:
-      nextX = 0;
-      nextY = 1;
-      break;
+    ctx.fillRect(position.x, position.y, size, size)
+  })
+}
+
+
+
+//Mover dentro da Grade
+const moveSnake = () => {
+  if (!direction) return
+
+  const head = snake[snake.length - 1]
+
+  if (direction == "right") {
+    snake.push({ x: head.x + size, y: head.y })
+  }
+
+  if (direction == "left") {
+    snake.push({ x: head.x - size, y: head.y })
+  }
+
+  if (direction == "down") {
+    snake.push({ x: head.x, y: head.y + size })
+  }
+
+  if (direction == "up") {
+    snake.push({ x: head.x, y: head.y - size })
+  }
+
+  snake.shift()
+}
+
+
+
+//Cria uma exibição de Grid para os jogadores visualizar a tela do jogo
+const drawGrid = () => {
+  ctx.lineWidth = 1
+  ctx.strokeStyle = "gray"
+
+  for (let i = 30; i < canvas.width; i += 30) {
+    ctx.beginPath()
+    ctx.lineTo(i, 0)
+    ctx.lineTo(i, 600)
+    ctx.stroke()
+
+    ctx.beginPath()
+    ctx.lineTo(0, i)
+    ctx.lineTo(600, i)
+    ctx.stroke()
   }
 }
 
 
-//Desenha a Snake-Run
 
-let defaultTamanhoCauda = 3;
-let tamanhoCauda = defaultTamanhoCauda;
-let caminhoCobra = [];
-let cobraEixoX = cobraEixoY = 10;
+//Criar uma verificação se houve contato com a head Snake toca a Apple
+const chackEat = () => {
+  const head = snake[snake.length - 1]
 
+  if (head.x == food.x && head.y == food.y) {
+    incrementScore()
+    snake.push(head)
+    /*
+    *audio.play()
+    *Não estou usando por definição do Projeto, Pessoal
+    */
+    let x = randomPosition()
+    let y = randomPosition()
 
+    while (snake.find((position) => position.x == x && position.y == y)) {
+      x = randomPosition()
+      y = randomPosition()
+    }
 
-//Criação da tela de jogo
-
-let tamanhoTela = tamanhoCaminho = 20;
-let nextX = nextY = 0;
-
-//Criando Comida
-
-let appleX = (appleY = 15);
-
-//"Update da Screen"
-
-function desenharJogo() {
-  cobraEixoX += nextX;
-  cobraEixoY += nextY;
-
-  if (cobraEixoX < 0) {
-    cobraEixoX = tamanhoTela - 1;
-  }
-
-  if (cobraEixoX > tamanhoTela - 1) {
-    cobraEixoX = 0;
-  }
-
-  if (cobraEixoY < 0) {
-    cobraEixoY = tamanhoTela - 1;
-  }
-
-  if (cobraEixoY > tamanhoTela - 1) {
-    cobraEixoY = 0;
-  }
-
-}
-
-//Se a cobra comer o alimento
-
-if (cobraEixoX == appleX && cobraEixoY == appleY) {
-  tamanhoCauda++;
-  appleX = Math.floor(Math.random() * tamanhoTela);
-  appleY = Math.floor(Math.random() * tamanhoTela);
-}
-
-//Snack to-go-to
-
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-ctx.fillStyle = "green";
-for (let i = 0; i < caminhoCobra.length; i++) {
-  ctx.fillRect(
-    caminhoCobra[i].x * tamanhoCaminho,
-    caminhoCobra[i].y * tamanhoCaminho,
-    tamanhoCaminho,
-    tamanhoCaminho
-  );
-  if (caminhoCobra[i].x == cobraEixoX && caminhoCobra[i].y == cobraEixoY) {
-    tamanhoCauda = defaultTamanhoCauda;
+    food.x = x
+    food.y = y
+    food.color = randomColor()
   }
 }
 
-ctx.fillStyle = "red";
-ctx.fillRect(appleX * tamanhoCaminho, appleY * tamanhoCaminho, tamanhoCaminho, tamanhoCaminho);
 
-caminhoCobra.push({
-  x: cobraEixoX,
-  y: cobraEixoY
-});
-while (caminhoCobra.length > tamanhoCauda) {
-  caminhoCobra.shift();
+
+//Criar uma condição para checar o limite da parede e caso a Snake toque e si
+const checkCollision = () => {
+  const head = snake[snake.length - 1]
+  const canvasLimit = canvas.width - size
+  const neckIndex = snake.length - 2
+
+  const wallCollision =
+    head.x < 0 || head.x > canvasLimit || head.y < 0 || head.y > canvasLimit
+
+  const selfCollision = snake.find((position, index) => {
+    return index < neckIndex && position.x == head.x && position.y == head.y
+  })
+
+  if (wallCollision || selfCollision) {
+    gameOver()
+  }
 }
-*/
+
+
+
+//Criar um Game Over, aparecendo na tela
+const gameOver = () => {
+  direction = undefined
+
+  menu.style.display = "flex"
+  finalScore.innerText = score.innerText
+  canvas.style.filter = "blur(2px)"
+}
+
+
+
+//Exibe as const's em tela para exibir o Game Snake
+const gameLoop = () => {
+  clearInterval(loopId)
+
+  ctx.clearRect(0, 0, 600, 600)
+  drawGrid()
+  drawFood()
+  moveSnake()
+  drawSnake()
+  chackEat()
+  checkCollision()
+
+  loopId = setTimeout(() => {
+    gameLoop()
+  }, 300)
+}
+
+
+
+//Criando a direção da Snake usando o teclado e definido, limite de tela
+gameLoop()
+
+document.addEventListener("keydown", ({ key }) => {
+  if (key == "ArrowRight" && direction != "left") {
+    direction = "right"
+  }
+
+  if (key == "ArrowLeft" && direction != "right") {
+    direction = "left"
+  }
+
+  if (key == "ArrowDown" && direction != "up") {
+    direction = "down"
+  }
+
+  if (key == "ArrowUp" && direction != "down") {
+    direction = "up"
+  }
+})
+
+
+
+//Criando o botão re-iniciar o Game Snake-Run2
+buttonPlay.addEventListener("click", () => {
+  score.innerText = "00"
+  menu.style.display = "none"
+  canvas.style.filter = "none"
+
+  snake = [initialPosition]
+})
